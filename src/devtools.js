47e830117cbdce1254b9064b3a07ff4537eb86dc
bramus <bramus@bram.us>
2025-02-07 23:32:42 +0100

    Apply workaround for loop problem
    @ref https://bugzilla.mozilla.org/show_bug.cgi?id=1852246#c5

--- src/devtools.js
+++ src/devtools.js
@@ -1,3 +1,12 @@
-chrome.devtools.panels.elements.createSidebarPane('POC', (sidebarPane) => {
+const initSidebarPanel = (sidebarPane) => {
     sidebarPane.setPage('devtools.html');
-});
+};
+
+// Only call this method when the script is running from the hidden devtools page registered from the manifest.json.
+if (window.location.pathname === "/devtools-page.html") {
+    chrome.devtools.panels.elements.createSidebarPane('POC', initSidebarPanel);
+} else {
+    // When running this script in the devtools sidebar page, set activePage to the current window
+    // (in the hidden devtools page that is going to be done by the initSidebarPanel callback).
+    activePane = window;
+}
